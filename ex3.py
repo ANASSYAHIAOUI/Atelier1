b5ebaeff0a4d8d1fa5835f5df237f6c5885ee753
ANASSYAHIAOUI <119661395+ANASSYAHIAOUI@users.noreply.github.com>
2022-12-07 14:09:03 +0100

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,6 +1,6 @@
 from ex2 import factorielle
 
-
+n=int(input(entrer un nombre))
 def sommeSerie(n):
     if(n<=0):
         return "le nombre doit être strictement positif"
@@ -11,3 +11,4 @@ def sommeSerie(n):
         for i in range(1,n+1):
             sum += factorielle(i-1)
         return sum
+print(sommeSerie(n))
